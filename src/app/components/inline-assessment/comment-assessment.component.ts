491b763716a0f2922934b7c330e3283f6110b030
alexseik <alexseik@hotmail.com>
2016-11-26 18:31:31 +0100

    Improve some styles on inline assessment

--- src/app/components/inline-assessment/comment-assessment.component.ts
+++ src/app/components/inline-assessment/comment-assessment.component.ts
@@ -6,12 +6,21 @@ import {Component, Input} from "@angular/core";
         <div class="panel" [hidden]="!hasVoted">
           <div class="panel-body">
             <h4>¿Porqué está {{tipo}}</h4>
-            {{texto}}
+            <span class="hideOverflow">{{texto}}</span>
             <textarea placeholder="máximo 250 caracteres ..." maxlength="250" style="width: 100%"></textarea>
             <button class="btn btn-success btn-block">Aceptar</button>
           </div>
         </div>
-    `
+    `,
+    styles: [`
+        .hideOverflow{
+            overflow:hidden;
+            white-space:nowrap;
+            text-overflow:ellipsis;
+            max-width: 200px;
+            display:block;
+        }
+    `]
 })
 
 export class CommentAssessment {
